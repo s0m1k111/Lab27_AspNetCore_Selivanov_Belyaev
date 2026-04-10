ab1a2339fa3bf908c029ad3f295bea1467bb55ed
s0m1k111 <spavel1207@gmail.com>
2026-04-10 16:36:20 +0300

    Step 9: complete mini-API server

--- WebServer/Program.cs
+++ WebServer/Program.cs
@@ -1,6 +1,8 @@
 using System.ComponentModel;
+using System.ComponentModel.Design.Serialization;
 using System.Data;
 using System.Data.Common;
+using System.IO.Pipelines;
 using System.Net.Security;
 using System.Security.Principal;
 using Microsoft.VisualBasic;
@@ -10,7 +12,7 @@ var app = builder.Build();
 
 // app.MapGet("/", () => "Привет от ИСП-233! Автор: <Селиванов Беляев>");
 
-app.Use(async (context, next) =>
+/*app.Use(async (context, next) =>
 {
     Console.WriteLine($"[LOG] {context.Request.Method} {context.Request.Path}");
     await next(context);
@@ -52,8 +54,46 @@ app.MapGet("/product/{id}", (int id) => new Product(
     Name: $"Товар #{id}",
     Price: id * 99.99m,
     InStock: id % 2 == 0
-));
+));*/
 
+app.Use(async (context, next) =>
+{
+    var method = context.Request.Method;
+    var path = context.Request.Path;
+    Console.WriteLine($"-> {method} {path}");
+    await next(context);
+});
+
+app.MapGet("/", () => Results.Ok(new
+{
+    Message = "Добро пожаловать",
+    Version = "1.0",
+    Time = DateTime.Now.ToString("HH:mm:ss")
+}));
+
+app.MapGet("/me", () => Results.Ok(new
+{
+    Name = "SelBel",
+    Group = "ISp-233",
+    Course = 3,
+    Skills = new[] { "c#", "html", "css", "js", "ASP.Net" }
+}));
+
+app.MapGet("/calc/{a}/{b}", (double a, double b) => Results.Ok(new
+{
+    A = a,
+    B = b,
+    Sum = a + b,
+    Diff = a - b,
+    Mul = a * b,
+    Div = b != 0 ? a / b : 0
+}));
+
+app.MapFallback(() => Results.NotFound(new
+{
+    Error = "Маршрут не найден",
+    Code = 404
+}));
 app.Run();
 
-record Product(int Id, string Name, decimal Price, bool InStock);
+//record Product(int Id, string Name, decimal Price, bool InStock);
